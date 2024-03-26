16302d4b90c6be45446a4c10a02b3f445eb8bea6
perrin1 <perrinfanou6@gmail.com>
2024-03-26 11:15:29 +0100

    Commite 1
    initailisation

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='bg-blue-500 p-4' >
+        <nav className='flex justify-between items-center max-w-4xl mx-auto' >
+            <Link href='/' className='text-white text-2xl font-bold ' >    
+                My Blog        
+            </Link>
+            <ul>
+                <li>
+                    <Link href='/api/auth/signin' className='text-white hover:underline ' >
+                        Login
+                    </Link>
+                </li>
+               
+            </ul>
+        </nav>
+    </header> 
+  )
+}
+ 
+export default Header
